dd8690d52e7172619c1acf8de0bb66bce2c42c72
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 21:11:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,15 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    
+
     now = Time.now
     today = Date.today.to_time
-    
+
     morning = today.beginning_of_day
     noon = today.noon
     evening = today.change( hour: 17 )
-    
-    
+
+
     if (morning..noon).cover? now
       resp.write 'Good Morning'
     else (noon..evening).cover? now
