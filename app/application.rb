2136644f4588cb83e9f0a3bd743b8caac788778a
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 20:42:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,13 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
 
+    current_time =
+
+    if current_time < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
   end
 end
