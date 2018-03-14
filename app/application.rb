6f2fcbaec953027bebc515606ac43fb4ee3b200c
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 21:28:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
 binding.pry
     if current_time.hour < 12
-      
+
       resp.write "Good Morning"
     elsif current_time.hour > 12 && current_time.hour < 24
       resp.write "Good Afternoon"
