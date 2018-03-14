9bc8c53246c2f9316cc3ddfc3f8e76200863da0e
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 21:26:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@ require 'pry'
 class Application
   def call(env)
     resp = Rack::Response.new
-binding.pry
+
     current_time = Time.new
 
-    case
-    when current_time.hour < 12
+
+    if current_time.hour < 12
       resp.write "Good Morning"
-    when current_time.hour > 12 && current_time.hour < 24
+    elsif current_time.hour > 12 && current_time.hour < 24
       resp.write "Good Afternoon"
     end
     resp.finish
